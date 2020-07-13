b7993613bcf9f5685fa16ce54582d04cc737ee38
Dawid Karasiewicz <dawid.karasiewicz@yougov.com>
2020-07-13 12:54:47 +0200

    change default port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 80;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port);
   });
