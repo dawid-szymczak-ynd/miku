25d3c50481e54f37dc37098fd37a9b9510ce6565
Dawid Karasiewicz <dawid.karasiewicz@yougov.com>
2020-07-15 16:07:47 +0200

    fix(*): add missing names - #9

--- tools/scripts/images-names.ts
+++ tools/scripts/images-names.ts
@@ -1,6 +1,8 @@
 export const imagesNames = {
   'miku-credit-front': 'miku-credit-front',
   api: 'api-gateway',
+  'book-keeper': 'book-keeper',
+  'subcriptio-muneris': 'subcriptio-muneris',
 };
 
 export type AppName = keyof typeof imagesNames;
