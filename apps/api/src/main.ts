93a6035dd42356add02e0cbb265880e2a7aa629e
Dawid Karasiewicz <dawid.karasiewicz@yougov.com>
2020-07-18 01:18:22 +0200

    chore(api): add swagger for api-gateway - #17

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { REDIS_CLIENT } from '@miku-credit/api/common';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import * as ConnectRedis from 'connect-redis';
 import * as ExpressSession from 'express-session';
@@ -10,11 +11,18 @@ import { AppModule } from './app/app.module';
 
 const DEFAULT_PORT = 80;
 const COOKIE_MAX_AGE = 1800000;
+const OPTIONS = new DocumentBuilder()
+  .setTitle('Api Gateway')
+  .setDescription('Api Gateway application public interface')
+  .setVersion('1.0')
+  .addTag('apiGateway')
+  .build();
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || DEFAULT_PORT;
   const redisClient = app.get(REDIS_CLIENT);
+  const document = SwaggerModule.createDocument(app, OPTIONS);
 
   app.use(
     ExpressSession({
@@ -33,6 +41,7 @@ async function bootstrap(): Promise<void> {
   );
   app.use(Passport.initialize());
   app.use(Passport.session());
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     Logger.log(`Listening at http://localhost:${port}`);
